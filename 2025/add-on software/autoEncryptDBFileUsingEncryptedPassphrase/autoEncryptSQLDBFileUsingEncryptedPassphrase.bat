9124b1205424ef0f14be9b7c2d0fb4bb9e3ddf47
SYSON, MICHAEL B. <masarapmabuhay@gmail.com>
2025-01-13 15:39:56 +0800

    Add files via upload

---
+++ 2025/add-on software/autoEncryptDBFileUsingEncryptedPassphrase/autoEncryptSQLDBFileUsingEncryptedPassphrase.bat
@@ -0,0 +1,64 @@
+@echo off
+REM
+REM Copyright 2019~2025 USBONG
+REM 
+REM Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You ' may obtain a copy of the License at
+REM
+REM http://www.apache.org/licenses/LICENSE-2.0
+REM
+REM Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, ' WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing ' permissions and limitations under the License.
+REM
+REM @company: USBONG
+REM @author: SYSON, MICHAEL B.
+REM @date created: 2019
+REM @date updated: 20250113; from 20250111
+REM @website address: http://www.usbong.ph
+REM
+echo //-----------------------------------------------------------------------
+echo //  Marikina Orthopedic Specialy Clinic - Sta. Lucia Health Care Centre
+echo //  USBONG
+echo //
+echo //  HTML File Automatic Encryption Using Encrypted Passphrase
+echo //
+echo //-----------------------------------------------------------------------
+echo.
+echo Verifying if ouput folder already exists...
+mkdir output
+echo.
+echo Automatically decrypting encrypted passphrase...
+set mainDirectory=%CD%
+
+REM edited by Mike, due to "rsautl" was deprecated in version 3.0
+REM openssl rsautl -decrypt -inkey %mainDirectory%/library/key.pem -in %mainDirectory%/library/pass.bin -out %mainDirectory%/output/pass.txt
+
+openssl pkeyutl -decrypt -inkey "%mainDirectory%/library/key.pem" -in "%mainDirectory%/library/pass.bin" -out "%mainDirectory%/output/pass.txt"
+
+echo.
+echo Automatically encrypting input HTML Files using decrypted passphrase...
+
+REM cd library
+
+REM outputs twice
+REM FOR /R .\input %i in (*.*) do echo %~nxi
+
+REM usbong_kms*Updated.sql
+
+cd input
+
+FOR %%i in (*.*) do set myFile=%%i
+
+REM echo ">>>>%myFile%"
+
+cd..
+
+cd library
+
+openssl enc -e -pbkdf2 -aes256 -in "%mainDirectory%/input/%myFile%" -out "%mainDirectory%/output/Encrypted_%myFile%" -kfile "%mainDirectory%/output/pass.txt"
+
+cd ..
+cd output
+del pass.txt
+cd ..
+
+REM explorer file:///%CD%/output/EncryptedMonthlySummaryTreatment.html
+REM explorer file:///%CD%/output/EncryptedMonthlySummaryConsultation.html
